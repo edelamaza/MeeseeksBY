81b44917e598d5134d220dd965ca7f11b6be6b30
rickyjassoa <43558401+rickyjasso@users.noreply.github.com>
2020-08-29 18:47:09 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,16 @@
-const seat = document.querySelectorAll(".asiento")
+const seat = document.querySelectorAll(".asiento");
+const porcentaje = document.querySelector("#porcentaje");
+
+porcentaje.addEventListener('mouseup', function(){
+    if (this.value > 0 && this.value < 50){
+        seat[0].classList.add("deleted");
+    } else{
+        seat[0].classList.remove("deleted");
+    }
+});
 
 for (let i = 0; i < seat.length; i++) {
     seat[i].addEventListener("click", function(){
-        seat[i].classList.toggle("deleted")
+        seat[i].classList.toggle("deleted");
     })
 }

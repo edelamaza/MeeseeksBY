3731a622bbd391c638e04773a10f4a31fe462995
rickyjassoa <43558401+rickyjasso@users.noreply.github.com>
2020-08-29 19:23:43 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,27 @@
 const seat = document.querySelectorAll(".asiento");
-const porcentaje = document.querySelector("#porcentaje");
+const porcentaje = document.getElementById("porcentaje");
 
-porcentaje.addEventListener('mouseup', function(){
-    if (this.value > 0 && this.value < 50){
+// porcentaje.addEventListener('mouseup', function(){
+//     if (this.value > 0 && this.value < 50){
+//         seat[0].classList.add("deleted");
+//     } else{
+//         seat[0].classList.remove("deleted");
+//     }
+// });
+
+porcentaje.addEventListener("input", function(){
+    let valorPorcentaje = document.getElementById("porcentaje").value;
+    let a = parseInt(valorPorcentaje);
+    if (a <= 50){
         seat[0].classList.add("deleted");
-    } else{
+    } else {
         seat[0].classList.remove("deleted");
     }
 });
 
+
 for (let i = 0; i < seat.length; i++) {
     seat[i].addEventListener("click", function(){
         seat[i].classList.toggle("deleted");
-    })
+    });
 }
